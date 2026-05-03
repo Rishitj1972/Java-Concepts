673c3ee9e1c06a1cc4aceef2a3ce3e2057f8caa8
Rishi <rishitj1972@gmail.com>
2026-05-03 19:05:40 +0530

    update

---
+++ DSA/Array Basic Probems/LeetCode Array Problems/RemoveElement.java
@@ -0,0 +1,20 @@
+class RemoveElement
+{
+    public static void main(String[] args) {
+        
+        int nums[] = {0,1,2,2,3,0,4,2};
+
+        int val = 2;
+        int idx = 0;
+
+        for(int i = 0 ; i < nums.length; i++ ) {
+
+            if(nums[i] != val) {
+                nums[idx] = nums[i];
+                idx++;
+            }
+        }
+
+        System.out.println(idx);
+    }
+}
